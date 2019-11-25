3c34a150cb8f93e90096440b7a46e4bb1d8c86c9
zozo <zahraa.jalaa99@gmail.com>
2019-11-25 22:55:53 +0300

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
   constructor() {
     super()
 
+
     this.state = {
       pets: [],
       filters: {
@@ -14,7 +15,22 @@ class App extends React.Component {
       }
     }
   }
-
+ fetchPets = () =>{
+   let petUrl = '/api/pets';
+   if(this.state.filters.type !== 'all'){petUrl +=`?type=${this.state.filters.type}`}
+   fetch(petUrl)
+   .then(res => res.json())
+   .then(pets => this.setState({pets}))
+ }
+ onChangeType = ({target:{value}}) => {
+  this.setState({filters:{...this.state.filters,type:value}})
+ }
+ onAdoptPet = (petId) => {
+   let pets = this.state.pets.map(pet =>{
+     return pet.id === petId ?{...pet,isAdopted:true}:pet;
+   })
+   this.setState({pets})
+ }
   render() {
     return (
       <div className="ui container">
@@ -24,10 +40,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
